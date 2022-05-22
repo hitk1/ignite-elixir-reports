ae113a06d6ae9b65fad0403441680b7f4db025e6
hitk1 <luispaulo.degini@gmail.com>
2022-05-22 17:22:42 -0300

    feat: test da funcao paralela

--- test/reports_generator_test.exs
+++ test/reports_generator_test.exs
@@ -97,4 +97,59 @@ defmodule ReportsGeneratorTest do
       assert status_result == expected_response
     end
   end
+
+  describe "build_from_many/1" do
+    test "When a file list is provided, build a report" do
+      file_names = ["report_test.csv", "report_test.csv"]
+
+      response = ReportsGenerator.build_from_many(file_names)
+
+      expected_response = %{
+        "foods" => %{
+          "a├ºa├¡" => 1,
+          "churrasco" => 2,
+          "esfirra" => 3,
+          "hamb├║rguer" => 2,
+          "pastel" => 0,
+          "pizza" => 2,
+          "prato_feito" => 0,
+          "sushi" => 0
+        },
+        "users" => %{
+          "1" => 48,
+          "10" => 36,
+          "11" => 0,
+          "12" => 0,
+          "13" => 0,
+          "14" => 0,
+          "15" => 0,
+          "16" => 0,
+          "17" => 0,
+          "18" => 0,
+          "19" => 0,
+          "2" => 45,
+          "20" => 0,
+          "21" => 0,
+          "22" => 0,
+          "23" => 0,
+          "24" => 0,
+          "25" => 0,
+          "26" => 0,
+          "27" => 0,
+          "28" => 0,
+          "29" => 0,
+          "3" => 31,
+          "30" => 0,
+          "4" => 42,
+          "5" => 49,
+          "6" => 18,
+          "7" => 27,
+          "8" => 25,
+          "9" => 24
+        }
+      }
+
+      assert is_map(response) == is_map(expected_response)
+    end
+  end
 end
